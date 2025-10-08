430a15d7925c9451be0a394039e4b471619fe543
Anniballi-Andrea <andreanniballi@gmail.com>
2025-10-08 16:29:49 +0200

    02 array 4 done

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -38,8 +38,11 @@ teachers.splice(5,1)
 console.log(teachers)
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
+
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+const isFabioPresent = teachers.indexOf('Fabio') >= 0;
+
+console.log(isFabioPresent)
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
 const teachersString = null;
